9d9dc763f08fd15ea51b3fc798d92ee48effbe15
Pipo <eliaducceschi@hotmail.it>
2025-05-15 13:30:14 +0200

    Adjust cleanup plugin

--- packages/cleanup-plugin/index.ts
+++ packages/cleanup-plugin/index.ts
@@ -1,5 +1,5 @@
-import {EditorPlugin, project, ui, data, Access} from '@wonderlandengine/editor-api';
-import {existsSync} from 'node:fs';
+import { Access, data, EditorPlugin, project, ui } from '@wonderlandengine/editor-api';
+import { existsSync } from 'node:fs';
 
 const EPSILON = 1e-6;
 const VEC3_ZERO = [0, 0, 0] as const;
@@ -9,13 +9,13 @@ const QUAT_IDENTITY = [0, 0, 0, 1] as const;
 /**
  * Plugin to cleanup resources with broken links.
  */
-export default class CleanupPlugin extends EditorPlugin {
+export default class PlaykoStudiosCleanupPlugin extends EditorPlugin {
     result: Record<string, string[]> = {};
 
     /* The constructor is called when your plugin is loaded */
     constructor() {
         super();
-        this.name = 'Project Cleanup Plugin';
+        this.name = 'PlaykoStudios Project Cleanup Plugin';
     }
 
     /* Use this function for drawing UI */
@@ -28,7 +28,7 @@ export default class CleanupPlugin extends EditorPlugin {
             return;
         }
 
-        for (let k of Object.keys(this.result)) {
+        for (const k of Object.keys(this.result)) {
             ui.text(`Found ${this.result[k].length.toString()} unused ${k}`);
         }
 
@@ -45,11 +45,12 @@ export default class CleanupPlugin extends EditorPlugin {
     LINK_CACHE: Record<string, boolean> = {};
 
     /* Check whether the file linked by a resource exists, caching the result */
-    linkExists(path: string) {
+    linkExists(path: string): boolean {
         if (!(path in this.LINK_CACHE)) {
             /* Try as relative to project root first then unprefixed in case it's an absolute path */
             this.LINK_CACHE[path] = existsSync(project.root + '/' + path) || existsSync(path);
         }
+
         return this.LINK_CACHE[path];
     }
 
@@ -59,7 +60,7 @@ export default class CleanupPlugin extends EditorPlugin {
         this.result = {};
         this.LINK_CACHE = {};
 
-        for (let res of [
+        for (const res of [
             'meshes',
             'textures',
             'materials',
@@ -67,9 +68,9 @@ export default class CleanupPlugin extends EditorPlugin {
             'animations',
             'skins',
         ]) {
-            const list = [];
-            for (const k of Object.keys(data[res])) {
-                const file = data[res][k].link?.file;
+            const list: string[] = [];
+            for (const k of Object.keys((data as any)[res])) {
+                const file = (data as any)[res][k].link?.file;
                 if (file && file !== 'default' && !this.linkExists(file)) {
                     list.push(k);
                 }
@@ -80,59 +81,14 @@ export default class CleanupPlugin extends EditorPlugin {
     }
 
     cleanup() {
-        for (let r of Object.keys(this.result)) {
-            for (let k of this.result[r]) {
-                delete data[r][k];
+        for (const r of Object.keys(this.result)) {
+            for (const k of this.result[r]) {
+                delete (data as any)[r][k];
             }
             console.log('Deleted', this.result[r].length, r);
         }
 
         this.collectResources();
-    }
-
-    private equalsEps(a: number, b: number) {
-        return Math.abs(a - b) < EPSILON;
-    }
-
-    private roundNumber(val: number) {
-        if (this.equalsEps(val, 0)) return 0;
-
-        const wantsNegative = val < 0;
-        const positiveVal = Math.abs(val);
-        const nearestPower = 2 ** Math.round(Math.log2(positiveVal));
-
-        if (this.equalsEps(positiveVal, nearestPower)) {
-            return wantsNegative ? -nearestPower : nearestPower;
-        } else {
-            return val;
-        }
-    }
-
-    private simplifyVector(access: Access, key: string, defaultVal: ReadonlyArray<number>, allowDelete: boolean) {
-        if (!access.exists(key)) return;
-
-        let isDefault = true;
-        let changed = false;
-        const val = [...access[key]];
-        for (let i = defaultVal.length - 1; i >= 0; i--) {
-            const newVal = this.roundNumber(val[i]);
-            if (newVal !== val[i]) {
-                val[i] = newVal;
-                changed = true;
-            }
-
-            if (!this.equalsEps(defaultVal[i], val[i])) isDefault = false;
-        }
-
-        if (allowDelete && isDefault) {
-            delete access[key];
-        } else if (changed) {
-            access[key] = val;
-        }
-    }
-
-    private simplifyFlag(access: Access, key: string, defaultVal: boolean) {
-        if (access.exists(key) && access[key] === defaultVal) delete access[key];
     }
 
     preProjectSave(): boolean {
@@ -145,7 +101,7 @@ export default class CleanupPlugin extends EditorPlugin {
             this.simplifyVector(object, 'scaling', VEC3_ONE, !isLinked);
             this.simplifyVector(object, 'rotation', QUAT_IDENTITY, !isLinked);
 
-            if (!object.exists('components')) continue;
+            if (!object.exists?.('components')) continue;
 
             for (let c = object.components.length; c >= 0; c--) {
                 const component = object.components[c];
@@ -163,10 +119,57 @@ export default class CleanupPlugin extends EditorPlugin {
             }
 
             if (!isLinked && object.components.length === 0) {
+                // @ts-expect-error: components is not optional, but we want to delete it anyway to remove it from the project
                 delete object.components;
                 continue;
             }
         }
+
         return true;
+    }
+
+    private equalsEps(a: number, b: number): boolean {
+        return Math.abs(a - b) < EPSILON;
+    }
+
+    private roundNumber(val: number): number {
+        if (this.equalsEps(val, 0)) return 0;
+
+        const wantsNegative = val < 0;
+        const positiveVal = Math.abs(val);
+        const nearestPower = 2 ** Math.round(Math.log2(positiveVal));
+
+        if (this.equalsEps(positiveVal, nearestPower)) {
+            return wantsNegative ? -nearestPower : nearestPower;
+        } else {
+            return val;
+        }
+    }
+
+    private simplifyVector(access: Access, key: string, defaultVal: ReadonlyArray<number>, allowDelete: boolean) {
+        if (!access.exists?.(key)) return;
+
+        let isDefault = true;
+        let changed = false;
+        const val = [...(access as any)[key]];
+        for (let i = defaultVal.length - 1; i >= 0; i--) {
+            const newVal = this.roundNumber(val[i]);
+            if (newVal !== val[i]) {
+                val[i] = newVal;
+                changed = true;
+            }
+
+            if (!this.equalsEps(defaultVal[i], val[i])) isDefault = false;
+        }
+
+        if (allowDelete && isDefault) {
+            delete (access as any)[key];
+        } else if (changed) {
+            (access as any)[key] = val;
+        }
+    }
+
+    private simplifyFlag(access: Access, key: string, defaultVal: boolean) {
+        if (access.exists?.(key) && (access as any)[key] === defaultVal) delete (access as any)[key];
     }
 }
